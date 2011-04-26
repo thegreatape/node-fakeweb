48f40a5c1976a8e1c0454818f30c9767502ee345
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2011-04-25 22:41:48 -0400

    initial import with stub code

---
+++ README.md
@@ -0,0 +1,3 @@
+An attempt to re-create the simple HTTP request interception in [chrisk's fakeweb][1] for testing modules that interface with external web services.
+
+[1]: https://github.com/chrisk/fakeweb
---
+++ fakeweb.js
@@ -0,0 +1,24 @@
+var http = require('http');
+
+http.intercept_rules = [];
+
+function match_rule(options){
+    var matched_rule;
+    http.intercept_rules.forEach(function(rule){
+        var keys = Object.keys(rule),
+            match = false;
+        // TODO headers matching and regex support
+        keys.forEach(function(key){
+            if(options[key]){ 
+                match = options[key] == rule[key];
+            }
+        });
+    });
+    return matched_rule;
+}
+
+// wrap http.request with interceptor function
+var old_request = http.request;
+http.request = function(options, callback){
+
+};
