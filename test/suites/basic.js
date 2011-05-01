f4ee89fdb3e78708457aa4402c4c5696af30a56c
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2011-05-01 18:03:52 -0400

    DRY out some of the test infrastructure

--- test/suites/basic.js
+++ test/suites/basic.js
@@ -3,23 +3,13 @@ var nodeunit = require('nodeunit'),
     utils = require('../utils'),
     http = require('http');
 
-STOCK_RESPONSE = "Hi, you've reached the target server!";
 module.exports = nodeunit.testCase({
-    setUp: function (callback) {
-        this.targetServer = http.createServer(function(req, res){
-            res.writeHead(200, {"Content-Type": "text/html"});
-            res.end(STOCK_RESPONSE);
-        });
-        this.targetServer.listen(3001, callback);
-    },
-    tearDown: function (callback) {
-        this.targetServer.close();
-        callback();
-    },
+    setUp: utils.setUp,
+    tearDown: utils.tearDown,
     test_basic_uri_interception: function(assert) {
         assert.expect(1);
         http.register_intercept({uri: '/', body: 'intercepted!'});
-        utils.request( { host: 'localhost', port: 3001, uri: '/', method: "GET"},
+        utils.request( { uri: '/'},
             function(res){
                 assert.equal(res, "intercepted!");
                 assert.done();
@@ -31,9 +21,9 @@ module.exports = nodeunit.testCase({
         var rule = {uri: '/', body: 'intercepted'};
         http.register_intercept(rule);
         http.unregister_intercept(rule);
-        utils.request( { host: 'localhost', port: 3001, uri: '/', method: "GET"},
+        utils.request( { uri: '/'},
             function(res){
-                assert.equal(res, STOCK_RESPONSE);
+                assert.equal(res, utils.STOCK_RESPONSE);
                 assert.done();
         });
     },
@@ -43,15 +33,15 @@ module.exports = nodeunit.testCase({
         http.register_intercept({uri: '/bar', body: 'bar'});
         http.register_intercept({uri: '/baz', body: 'baz'});
         http.clear_intercepts();
-        utils.request({uri: '/', port: 3001, method: "GET", host: 'localhost'},
+        utils.request({uri: '/'},
                 function(res){
-                    assert.equal(res, STOCK_RESPONSE);
-                    utils.request({uri: '/bar', port: 3001, method: "GET", host: 'localhost'},
+                    assert.equal(res, utils.STOCK_RESPONSE);
+                    utils.request({uri: '/bar'},
                         function(res){
-                            assert.equal(res, STOCK_RESPONSE);
-                            utils.request({uri: '/baz', port: 3001, method: "GET", host: 'localhost'},
+                            assert.equal(res, utils.STOCK_RESPONSE);
+                            utils.request({uri: '/baz'},
                             function(res){
-                                assert.equal(res, STOCK_RESPONSE);
+                                assert.equal(res, utils.STOCK_RESPONSE);
                                 assert.done();
                             });
                         });
--- test/utils.js
+++ test/utils.js
@@ -1,12 +1,30 @@
-var http = require('http');
+var http = require('http'),
+    utils = module.exports;
 
-module.exports.request = function(options, cb){
+utils.request = function(options, cb){
+    options.host = options.host || 'localhost';
+    options.port = options.port || 3001;
+    options.method = options.method || 'GET';
     var req = http.request( options, function(response){
-            var res = '';
-            response.on('data', function(i){ res+=i;});
-            response.on('end', function(){
-                cb(res);
-            });
+        var res = '';
+        response.on('data', function(i){ res+=i;});
+        response.on('end', function(){
+            cb(res);
+        });
     });
     req.end();
 }
+
+utils.STOCK_RESPONSE = "Hi, you've reached the target server!";
+utils.setUp = function (callback) {
+    this.targetServer = http.createServer(function(req, res){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(utils.STOCK_RESPONSE);
+    });
+    this.targetServer.listen(3001, callback);
+}
+
+utils.tearDown = function (callback) {
+        this.targetServer.close();
+        callback();
+    }
